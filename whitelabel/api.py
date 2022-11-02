57b1f462d8ff896fea370671393e76c602492f6f
Richard Rattary <narutoksan@gmail.com>
2022-11-02 16:38:59 -0400

    Update api.py
    fix a nonetype string issue

--- whitelabel/api.py
+++ whitelabel/api.py
@@ -20,7 +20,9 @@ def update_field_label():
 	frappe.db.sql("""Update `tabDocField` set label='ERP' where fieldname='erpnext_user' and parent='Employee'""")
 
 def get_frappe_version():
-	return frappe.db.get_value("Installed Application",{"app_name":"frappe"},"app_version").split('.')[0]
+  frappe_version = next((item for item in frappe.utils.get_installed_apps_info() if item['app_name'] == 'frappe'), '13.0')
+  return frappe_version['version'].split('.')[0]
+	# return frappe.db.get_value("Installed Application",{"app_name":"frappe"},"app_version").split('.')[0]
 
 def update_onboard_details():
 	update_onboard_module()
